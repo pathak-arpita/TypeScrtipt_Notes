033df0ecc12497e91792dea4ccf22c6774e4fc14
pathak-arpita <pathak.arpita6497@gmail.com>
2025-06-16 15:08:53 +0530

    adding useState file

--- TypeScript-in-React/src/App.tsx
+++ TypeScript-in-React/src/App.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import Values_As_Props from './Components/Passing_Props/Values_As_Props/Values_As_Props';
 import Object_As_Prop from './Components/Passing_Props/Object_As_Prop/Object_As_Prop';
 import Array_As_Props from './Components/Passing_Props/Array_As_Props/Array_As_Props';
 import Advanced_Props from './Components/Passing_Props/Advanced_Props/Advanced_Props';
+import UseState from './Components/UseState/UseState';
 
 
 function App() {
@@ -10,10 +12,13 @@ function App() {
     <>
       {/* Passing Props */}
       <div>
-       <Values_As_Props />
-       <Object_As_Prop />
-       <Array_As_Props />
-       <Advanced_Props />
+        {/* <Values_As_Props />
+        <Object_As_Prop />
+        <Array_As_Props />
+        <Advanced_Props /> */}
+
+        {/* Using useState Hook */}
+        <UseState />
       </div>
     </>
   )
--- TypeScript-in-React/src/Components/UseState/UseState.tsx
+++ TypeScript-in-React/src/Components/UseState/UseState.tsx
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
 const UseState = () => {
+    const [count, setCount] = useState<number>(0);
+    const [name, setName] = useState<string>("Value is Zero");
+    const [isZero, setIsZero] = useState<boolean>(true);
+
+    const increment = () => {
+        setCount(prevCount => {
+            const newCount = prevCount + 1;
+            setName(newCount > 0 ? "Value is Positive" : newCount < 0 ? "Value is Negative" : "Value is Zero");
+            setIsZero(newCount === 0);
+            return newCount;
+        });
+    };
+
+    const decrement = () => {
+        setCount(prevCount => {
+            const newCount = prevCount - 1;
+            setName(newCount > 0 ? "Value is Positive" : newCount < 0 ? "Value is Negative" : "Value is Zero");
+            setIsZero(newCount === 0);
+            return newCount;
+        });
+    };
+
     return (
-        <h1>Hello</h1>
-    )
-}
+        <>
+            <h2>UseState :</h2>
+            <p style={{ fontSize: "20px", color: "blue" }}>{count}</p>
+            <p style={{ fontSize: "20px", color: "blue" }}>{name}</p>
+            <p style={{ fontSize: "20px", color: "blue" }}>{isZero ? "Number is ZERO" : "Number is NOT ZERO"}</p>
+            <button onClick={increment}>Increment</button>&nbsp;&nbsp;&nbsp;
+            <button onClick={decrement}>Decrement</button>
+        </>
+    );
+};
 
 export default UseState;
